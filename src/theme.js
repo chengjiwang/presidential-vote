bea931dafdfe8986529bb9d112a4fd44419f3099
Alvin Wang <chengji3728@gmail.com>
2024-04-26 16:06:12 +0800

    refactor: Update layout structure

--- src/theme.js
+++ src/theme.js
@@ -13,7 +13,17 @@ const theme = createTheme({
     bg: {
       main: '#E9ECEF',
       contrastText: '#334155',
-    }
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400,
+    },
   },
 });
 
